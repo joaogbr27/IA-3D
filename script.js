001ca0d31cc65abda0ae631755dcb44cd83556d1
joaogbr27 <127787793+joaogbr27@users.noreply.github.com>
2024-08-02 07:45:02 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+const caixaPrincipal = documen.querySelector('caixa-principal');
+const caixaPergunta = documen.querySelector('caixa-pergunta');
+const caixaalternativa = documen.querySelector('caixa-alternativa');
+const caixaresultado = documen.querySelector('caixa-resultado');
+
+const perguntas =[
+    {
+        enunciado:"pergunta 1",
+        alternativas:[
+            "alernativa 1",
+            "alternativa 2"
+        ]
+    }
+]
