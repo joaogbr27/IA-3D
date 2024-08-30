6b427552cc1b44a228e3ee7beb8fb7a1d2a00362
joaogbr27 <127787793+joaogbr27@users.noreply.github.com>
2024-08-30 07:21:38 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -5,15 +5,15 @@ const caixaResultado = document.querySelector('.caixa-resultado');
 
 const perguntas = [
     {
-        enunciado:"Pergunta 1",
+        enunciado: "Pergunta 1",
         alternativas:["Alternativa 1","Alternativa 2"]
     },
     {
-        enunciado:"Pergunta 2",
+        enunciado: "Pergunta 2",
         alternativas:["Alternativa 1","Alternativa 2"]
     },
     {
-        enunciado:"Pergunta 3",
+        enunciado: "Pergunta 3",
         alternativas:["Alternativa 1","Alternativa 2"]
     }
 ]
@@ -22,5 +22,6 @@ let atual = 0;
 let perguntaAtual;
 
 function mostraPergunta(){
-    
+    perguntaAtual = perguntas[atual];
+    caixaPergunta.textContent = perguntaAtual.enunciado;
 }
